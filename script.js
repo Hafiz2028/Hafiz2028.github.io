c377b44ef17142a400e2d0be4777da4cf1ac7d8e
Hafiz2028 <91659690+Hafiz2028@users.noreply.github.com>
2023-12-04 23:08:38 +0700

    script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Aaaaa, I like you too";
+  question.innerHTML = "Aaaaa, Met Lvl Up Amiiii <3";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
